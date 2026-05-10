c6d858c5f8adf00fa70bdb0bfc847eedee99cef3
MarkoPetr <markopetrovic867@yahoo.com>
2026-05-10 04:44:28 +0200

    Remove all files

--- android/app/src/main/java/com/salen/dzejlan/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.salen.dzejlan;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.salen.dzenanwin',
-  appName: 'DzenanWin',
-  webDir: 'build'
-};
-
-export default config;
