475faf8fed93b077cda5d7a266bf2c901d724d0b
MarkoPetr <markopetrovic867@yahoo.com>
2026-05-10 05:02:50 +0200

    Capture matches JSON

--- mozzart_human.py
+++ mozzart_human.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright
 import json
 
-all_urls = []
+matches_data = []
 
 with sync_playwright() as p:
     browser = p.chromium.launch(
@@ -14,12 +14,21 @@ with sync_playwright() as p:
     page = context.new_page()
 
     def log_response(resp):
-        url = resp.url
+        if "https://www.mozzartbet.com/betting/matches" in resp.url:
+            print("🎯 FOUND MATCHES API")
 
-        if "api" in url or "match" in url or "bet" in url:
-            print("API:", url)
+            try:
+                data = resp.json()
 
-            all_urls.append(url)
+                matches_data.append({
+                    "url": resp.url,
+                    "data": data
+                })
+
+                print("✅ JSON captured")
+
+            except Exception as e:
+                print("❌ JSON ERROR:", e)
 
     page.on("response", log_response)
 
@@ -31,8 +40,8 @@ with sync_playwright() as p:
     page.wait_for_timeout(15000)
 
     with open("matches.json", "w", encoding="utf-8") as f:
-        json.dump(all_urls, f, ensure_ascii=False, indent=2)
+        json.dump(matches_data, f, ensure_ascii=False, indent=2)
 
-    print("Saved:", len(all_urls))
+    print("Saved responses:", len(matches_data))
 
     browser.close()
